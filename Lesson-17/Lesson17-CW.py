5906b0a1e6585f5530942c9050aeadf00da7c1a2
Mimoona <memoona.raheel8@gmail.com>
2020-07-19 01:36:50 +0200

    Create Lesson17-CW.py

---
+++ Lesson-17/Lesson17-CW.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 18 20:02:11 2020
+
+@author: Mimoona Raheel
+"""
+
+print('Excercise: 1')
+print("~~~~~~~~~~~~")
+
+import math
+x = 2
+y = 4
+
+# calculate a = square root of (x+y=
+a = math.sqrt(x+y)
+print(a)
+
+
+#%%
+
+print('Ecercise: 2')
+print("~~~~~~~~~~~~")
+
+import matplotlib.pyplot as plt
+
+plt.bar(x=[3,4,5,6], height=[1,0.5,1.2,0.8], width=0.2, align='center')
+plt.show()
+
+
+
+#%%
+
+print('Excercise: 3')
+print("~~~~~~~~~~~~")
+
+import random
+
+for i in range(0,21):
+    print(random.randint(1,21))
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
